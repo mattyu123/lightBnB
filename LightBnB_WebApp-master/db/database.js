ee81b08cb0a320812bdf240166b1015cd38f033a
mattyu123 <matthewyu100@gmail.com>
2023-07-11 19:57:47 -0400

    refactor addNewProperty so user can list a new property

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -74,7 +74,7 @@ const addUser = function(user) {
 
 /// Reservations
 //function that pulls a guests reservation info based on their ID 
-const getAllReservations = function(guest_id) {
+const getAllReservations = function(guest_id, limit=10) {
   const queryCode = `
   SELECT reservations.id,
   properties.*,
@@ -96,9 +96,9 @@ const getAllReservations = function(guest_id) {
 
   ORDER BY reservations.start_date 
 
-  LIMIT 10;
+  LIMIT $2;
   `
-  return pool.query(queryCode, [guest_id])
+  return pool.query(queryCode, [guest_id, limit])
     .then(res => {
       return res.rows;
     })
@@ -107,57 +107,104 @@ const getAllReservations = function(guest_id) {
     })
 }
 
-
-/**
-//  * Get all reservations for a single user.
-//  * @param {string} guest_id The id of the user.
-//  * @return {Promise<[{}]>} A promise to the reservations.
-//  */
-// const getAllReservations = function (guest_id, limit = 10) {
-//   return getAllProperties(null, 2);
-// };
-
-/// Properties
-
-/**
- * Get all properties.
- * @param {{}} options An object containing query options.
- * @param {*} limit The number of results to return.
- * @return {Promise<[{}]>}  A promise to the properties.
- */
-
-
-//function that pulls the list of all the properties
+//New getAllProperties function - This will dynamically filter out the code 
 const getAllProperties = function(options, limit = 10) {
   const run = `
-  SELECT *
+  SELECT properties.id,
+  title,
+  cost_per_night,
+  AVG(property_reviews.rating) AS average_rating
+
   FROM properties
-  LIMIT $1
+
+  INNER JOIN property_reviews
+  ON properties.id = property_reviews.property_id
+
+  WHERE city = 'Vancouver'
+
+  GROUP BY properties.id
+
+  HAVING AVG(property_reviews.rating) >= 4
+
+  ORDER BY cost_per_night ASC
+
+  LIMIT 10;
   `
 
   return pool
-    .query(run, [limit])
-    .then((res) => {
-      return res.rows;
-      }
-    )
+    .query(run)
+    .then()
     .catch((err) => {
-      console.log("There is an error", err);
+      console.log("There is an error", err)
     })
-  };
+}
 
 
-/**
- * Add a property to the database
- * @param {{}} property An object containing all of the property details.
- * @return {Promise<{}>} A promise to the property.
- */
+/// Properties
+//function that pulls the list of all the properties -- Old code for getAll Properties
+// const getAllProperties = function(options, limit = 10) {
+//   const run = `
+//   SELECT *
+//   FROM properties
+//   LIMIT $1
+//   `
+//   return pool
+//     .query(run, [limit])
+//     .then((res) => {
+//       return res.rows;
+//       }
+//     )
+//     .catch((err) => {
+//       console.log("There is an error", err);
+//     })
+//   };
+
+//Function that will add a new item to properties
 const addProperty = function (property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-};
+  const title = property.title
+  const description = property.description
+  const thumbnail_photo_url = property.thumbnail_photo_url
+  const cover_photo_url = property.cover_photo_url
+  const cost_per_night = property.cost_per_night
+  const street = property.street
+  const city = property.city
+  const province = property.province
+  const post_code = property.post_code
+  const country = property.country
+  const parking_spaces = property.parking_spaces
+  const number_of_bathrooms = property.number_of_bathrooms
+  const number_of_bedrooms = property.number_of_bedrooms
+
+  const queryCode = 
+  `
+  INSERT INTO properties (title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13)
+  RETURNING *;
+  `
+
+  return pool
+    .query(queryCode, [title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms])
+    .then(res => {
+      return res.rows[0];
+    })
+    .catch(err => {
+      console.log("There is an error", err)
+    })
+}
+
+///OLD ADD NEW PROPERTY FUNCTION
+
+// /**
+//  * Add a property to the database
+//  * @param {{}} property An object containing all of the property details.
+//  * @return {Promise<{}>} A promise to the property.
+//  */
+// const addProperty = function (property) {
+//   const propertyId = Object.keys(properties).length + 1;
+//   property.id = propertyId;
+//   properties[propertyId] = property;
+//   return Promise.resolve(property);
+// };
 
 module.exports = {
   getUserWithEmail,
